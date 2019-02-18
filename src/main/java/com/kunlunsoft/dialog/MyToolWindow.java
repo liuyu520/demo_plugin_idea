5137900bba353607d0d46fedc4f715cae1ea96de
hanjun.hw <hanjun.hw@alibaba-inc.com>
2019-02-18 20:06:45 +0800

    增加 webview

--- src/main/java/com/kunlunsoft/dialog/MyToolWindow.java
+++ src/main/java/com/kunlunsoft/dialog/MyToolWindow.java
@@ -3,10 +3,16 @@ package com.kunlunsoft.dialog;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
-import com.intellij.ui.content.Content;
-import com.intellij.ui.content.ContentFactory;
-import com.kunlunsoft.form.ToolWindowForm;
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.web.WebView;
 import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
 
 public class MyToolWindow implements ToolWindowFactory {
     /**
@@ -17,10 +23,37 @@ public class MyToolWindow implements ToolWindowFactory {
      */
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
-        ToolWindowForm myToolWindow = new ToolWindowForm(toolWindow, project);
+        /*ToolWindowForm myToolWindow = new ToolWindowForm(toolWindow, project);
         ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
         Content content = contentFactory.createContent(myToolWindow.getRootPanel(), "", false);
-        toolWindow.getContentManager().addContent(content);
+        toolWindow.getContentManager().addContent(content);*/
+
+
+        final JFXPanel fxPanel = new JFXPanel();
+        JComponent component = toolWindow.getComponent();
+
+        Platform.setImplicitExit(false);
+        Platform.runLater(() -> {
+            Group root = new Group();
+//            Scene scene  =  new  Scene(root, Color.ALICEBLUE);
+            Text text = new Text();
+
+            text.setX(40);
+            text.setY(100);
+            text.setFont(new Font(25));
+            text.setText("Welcome JavaFX!");
+
+            root.getChildren().add(text);
+
+            WebView webView = new WebView();
+            webView.getEngine().load("https://bpms-test.alibaba-inc.com/processdesigner/newProcDesign?processId=807764092");
+
+            fxPanel.setScene(new Scene(webView));
+
+//            fxPanel.setScene(scene);
+        });
+
+        component.getParent().add(fxPanel);
     }
 
     @Override
